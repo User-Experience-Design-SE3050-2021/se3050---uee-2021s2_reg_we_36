5c9dddd0ed36592a3aa5190661971e6f2ed6ecce
senith sulara <senithsula00@gmail.com>
2021-10-02 15:42:43 +0530

    Exam Result view

--- app/src/main/java/com/example/schoolmanagment/TeacherHome.java
+++ app/src/main/java/com/example/schoolmanagment/TeacherHome.java
@@ -9,12 +9,22 @@ import android.widget.Button;
 
 public class TeacherHome extends AppCompatActivity {
 
+    Button examView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_home);
         getSupportActionBar().hide();
 
+        examView = findViewById(R.id.btnexam);
+
+        examView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), ResultVIew.class);
+                startActivity(i);
+            }
+        });
 
     }
 }
