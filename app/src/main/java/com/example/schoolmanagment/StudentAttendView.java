24c6c67c8cca94da435e469b137e6f6ee6985138
Pasindu Chathuranga <it19099132@my.sliit,lk>
2021-10-10 15:07:43 +0530

    date picker

--- app/src/main/java/com/example/schoolmanagment/StudentAttendView.java
+++ app/src/main/java/com/example/schoolmanagment/StudentAttendView.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ImageView;
 
 import butterknife.BindView;
@@ -13,6 +15,10 @@ public class StudentAttendView extends AppCompatActivity {
     //import back button
     @BindView(R.id.btnBack)
     ImageView backBtn;
+    //date picker
+    @BindView(R.id.shwDate)
+    Button shwDate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,5 +27,22 @@ public class StudentAttendView extends AppCompatActivity {
         //back button code
         ButterKnife.bind(this);
         backBtn.setOnClickListener(view -> finish());
+
+        //date picker
+        DatePicker datePicker = (DatePicker)findViewById(R.id.simpleDatePicker); // initiate a date picker
+
+        datePicker.setSpinnersShown(false); // set false value for the spinner shown function
+
+        //show hide date picker
+        shwDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (datePicker.getVisibility() == View.GONE){
+                    datePicker.setVisibility(View.VISIBLE);
+                }else{
+                    datePicker.setVisibility(View.GONE);
+                }
+            }
+        });
     }
 }
